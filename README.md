b3d4457692f7dba124dbcc7cf6021317633af3dc
showercapNA <nanaksya@utexas.edu>
2021-11-02 11:34:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-5p3xks
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-5p3xks)
---
+++ index.js
@@ -0,0 +1,17 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+//This is Nana's Javascript assignment
+
+var myteam;
+console.log(myteam); 
+myteam = "rockets"; 
+
+
+
+
+
+
